5a40d0e061c663b8a75a39e0c5439e0bba6d2a52
Alex Rabin <alexrabin@me.com>
2022-04-04 20:43:47 -0700

    Almost done with gui

---
+++ quotes_controller.py
@@ -0,0 +1,50 @@
+import json
+
+class QuotesController:
+
+    def __init__(self):
+        quotesFile = open("/home/pi/MagicMirror/modules/MMM-CloneWarsQuotes/MMM-MotivationQuotes.json", "r");
+        self._quotes = json.load(quotesFile)
+        quotesFile.close()
+
+    def updateQuotesFile(self, quotesList):
+        quotesFile = open("/home/pi/MagicMirror/modules/MMM-CloneWarsQuotes/MMM-MotivationQuotes.json", "w")
+        json.dump(quotesList, quotesFile, indent=2)
+        quotesFile.close()
+
+    def addQuote(self,newQuote):
+        if newQuote in self._quotes:
+            return False
+        self._quotes.append(newQuote)
+
+        self.updateQuotesFile(self._quotes)
+        return True
+
+    def editQuote(self, index, newQuote):
+        if (int(index) > newQuote.length):
+            return False
+
+        self._quotes[index] = newQuote;
+    def removeQuote(self, quoteToRemove):
+        if (quoteToRemove.isnumeric() == True):
+            return False
+
+        if quoteToRemove not in self._quotes:
+            return False
+
+        self._quotes.remove(quoteToRemove)
+        self.updateQuotesFile(self._quotes)
+        return True
+
+    def removeQuoteWithIndex(self,index):
+        if (index.isnumeric() == False):
+            return False
+
+        if (int(index) > len(self._quotes) - 1):
+            return False
+
+        quote = self._quotes[int(index)]
+        return removeQuote(quote)
+
+    def getQuotes(self):
+        return self._quotes
--- quotes_gui.py
+++ quotes_gui.py
@@ -1,23 +1,76 @@
-from guizero import App, ListBox
+from guizero import App, ListBox, TextBox, Box, PushButton, yesno, question, info, error
 import json
+import quotes_controller as q
 
-def getRawQuotes():
-    quotesFile = open("/home/pi/MagicMirror/modules/MMM-CloneWarsQuotes/MMM-MotivationQuotes.json", "r");
-    jsonObject = json.load(quotesFile)
-    quotesFile.close()
-    return jsonObject
 
-def quoteSelected(option):
-    print(option)
+quotesController = q.QuotesController()
+
+def getQuotes():
+    rawQuotesList = quotesController.getQuotes()
+    formatedQuotes = []
+    for i in range(len(rawQuotesList)):
+        q = rawQuotesList[i]
+        formatedQuotes.append(f"{i + 1}.) "+ q)
+    return formatedQuotes
 
 app = App(title="Quotes")
 
-rawQuotesList = getRawQuotes()
-formatedQuotes = []
-for i in range(len(rawQuotesList)):
-    q = rawQuotesList[i]
-    formatedQuotes.append(f"{i + 1}.) "+ q)
 
-ListBox(app, items=formatedQuotes, width="fill", command=quoteSelected)
+currentQuoteBox = Box(app, width="fill", height=120, visible=False)
+currentQuote = None
+listBox = None
+
+def addQuote():
+    response = question("Add Quote", None);
+    if (response is not None):
+        if (quotesController.addQuote(response)):
+            info("Success", "Succesfully add quote\n\n" + response)
+            listBox.clear()
+            for q in getQuotes():
+                listBox.append(q)
+        else:
+            error("Error", "Failed to save quote")
+
+addButton = PushButton(app, text="New Quote", command=addQuote)
+addButton.bg = "green"
+addButton.text_color = "white"
+def editQuoteAction():
+    print(currentQuote.value)
+    response = question("Edit Quote", None, initial_value=currentQuote.value[:len(currentQuote.value)-1])
+    if (response is not None):
+
+        quotesController.addQuote(response)
+        listBox.clear()
+        for q in getQuotes():
+            listBox.append(q)
+
+
+def deleteQuoteAction():
+    v = currentQuote.value[:len(currentQuote.value)-1]
+    response = yesno("Delete?", "Are you sure you want to delete this quote?\n\n'" + v + "'")
+    if (response == True):
+        if (quotesController.removeQuote(v)):
+            info("Success", "Succesfully removed quote\n\n" + v)
+            currentQuoteBox.hide()
+
+
+buttonGroup = Box(currentQuoteBox, layout="grid", height=50, width=120)
+editButton = PushButton(buttonGroup, text="Edit", command=editQuoteAction, grid=[0,0])
+deleteButton = PushButton(buttonGroup, text="Delete", command=deleteQuoteAction, grid=[1,0])
+deleteButton.bg = "red"
+deleteButton.text_color ="white"
+currentQuote = TextBox(currentQuoteBox, width="fill", height=70, multiline=True)
+
+
+def quoteSelected(option):
+    currentQuote.enable()
+    q = option.split(") ")[1]
+    currentQuote.value = q
+    if (currentQuoteBox.visible == False):
+        currentQuoteBox.show()
+    currentQuote.disable()
+
+listBox = ListBox(app, items=getQuotes(), width="fill", command=quoteSelected, scrollbar=True)
+
 
 app.display()
